56c1d687afa3968cec744f0d7443e1d1fedaaa86
hyperkarma <randy_pan@yahoo.com>
2020-09-17 20:02:47 +0100

    Updates

---
+++ precache-manifest.d467427f20f579301015647e1af60a2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f971824920a84842155b6619122d1905",
+    "url": "/google-books-client/index.html"
+  },
+  {
+    "revision": "7fddd4d87d2b9ef114bc",
+    "url": "/google-books-client/static/js/2.cb3f86db.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/google-books-client/static/js/2.cb3f86db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6d2ef376126b36cdf23",
+    "url": "/google-books-client/static/js/main.bb8615af.chunk.js"
+  },
+  {
+    "revision": "2f4606b7ccc9863de05f",
+    "url": "/google-books-client/static/js/runtime-main.7a2912b8.js"
+  }
+]);
