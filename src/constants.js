188da8e29474b5522b45758b2f9d785b1fdee83a
hyperkarma <randy_pan@yahoo.com>
2020-09-24 18:37:47 +0100

    url in constant for Heroku changed

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 export const BASE_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:8000"
-    : "https://immense-wave-64262.herokuapp.com";
+    : "https://calm-fjord-01191.herokuapp.com";
